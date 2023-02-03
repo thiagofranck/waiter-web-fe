0aab529324c8b8c1b936604b683be3f3aa4b097b
Thiago Franck <thiagohenrique776@gmail.com>
2023-02-02 23:14:55 -0300

    feat: add global styles and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
 export function App() {
-  return <h1>WaiterApp</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>WaiterApp</h1>
+    </>
+  );
 }
