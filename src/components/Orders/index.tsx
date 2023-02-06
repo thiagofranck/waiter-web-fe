e93a7cc3b0e794c6b2f4549a8f89fdfd3dba14a7
Thiago Franck <thiagohenrique776@gmail.com>
2023-02-05 23:32:41 -0300

    feat: add orders and ordersboard components

---
+++ src/components/Orders/index.tsx
@@ -0,0 +1,14 @@
+import { OrdersBoard } from '../OrdersBoard';
+
+import { Container } from './styles';
+
+export function Orders() {
+  return (
+    <Container>
+      <OrdersBoard />
+      <OrdersBoard />
+      <OrdersBoard />
+
+    </Container>
+  );
+}
---
+++ src/components/OrdersBoard/styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+
+export const Board = styled.div`
+  padding: 1rem;
+  border: 1px solid rgba(204, 204, 204, 0.4);
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+
+  header {
+    padding: 0.5rem;
+    font-size: 0.875rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+`;
+
+export const OrdersContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin-top: 1.5rem;
+
+  button {
+    background: #FFF;
+    border: 1px solid rgba(204, 204, 204, 0.4);
+    height: 8rem;
+    border-radius: 8px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.25rem;
+
+    strong {
+      font-weight: 500;
+    }
+
+    span {
+      font-size: 0.875rem;
+      color: #666;
+    }
+
+    & + button {
+      margin-top: 1.5rem;
+    }
+  }
+`;
